4293709b77f03f9fe90e50c2ce8bc2e93f1fca72
Dean Blackketter <dean@blackketter.com>
2016-09-25 15:21:32 -0700

    Switch is in its own library, soon to be moved to github Starting to use myWifi library.

--- src/Razzle.ino
+++ src/Razzle.ino
@@ -1,35 +1,11 @@
 #include <Arduino.h>
-#include <ESP8266WiFi.h>
-#include <ESP8266mDNS.h>
-#include <ESP8266HTTPClient.h>
-#include <NTPClient.h>
-#include <WiFiUdp.h>
-#include <ArduinoOTA.h>
-
-#include "Switch.h"
+#include <Switch.h>
 #include <Encoder.h>
+#include <TimeLib.h>
 
-#include "TimeLib.h"
+#include <myWifi.h>
 
 #include "RazzleLeds.h"
-
-const char* ssid = "bkn";
-const char* password = "5d4bf72344";
-const char* myHostname;
-
-struct devInfo {
-  const char* mac;
-  const char* hostname;
-  int numLeds;
-  EOrder colorOrder;
-//  byte ambient_anode;
-//  byte ambient_cathode;
-};
-
-// By default 'time.nist.gov' is used with 60 seconds update interval and
-// no offset
-WiFiUDP ntpUDP;
-NTPClient timeClient(ntpUDP);
 
 #define BUTTON_PIN (D6)
 Switch button = Switch(BUTTON_PIN);  // Switch between a digital pin and GND
@@ -38,20 +14,21 @@ Switch button = Switch(BUTTON_PIN);  // Switch between a digital pin and GND
 #define ENCODER_B_PIN (D8)
 Encoder knob(ENCODER_A_PIN,ENCODER_B_PIN);
 
-#define LIGHT_SENSOR_LED_N_PIN (D0)
-#define LIGHT_SENSOR_LED_P_PIN (D5)
-
-//LEDAmbientSensor* ambient = nullptr;
-
 bool firstRun = true;
 bool recoverMode = false;
-bool networkUp = false;
+
+struct devInfo {
+  const char* mac;
+  const char* hostname;
+  int numLeds;
+  EOrder colorOrder;
+};
 
 devInfo devices[] = {
-  { "5C:CF:7F:18:EA:42", "RazzleButton", 1, RGB },
+  { "5C:CF:7F:18:EA:42", "RazzleButton",  1, RGB },
   { "5C:CF:7F:10:4C:43", "RazzleString", 50, RGB },
-  { "5C:CF:7F:16:E6:EC", "RazzleBox", 64, GRB },
-  { nullptr, "RazzleUndef", 1, RGB }
+  { "5C:CF:7F:16:E6:EC", "RazzleBox",    64, GRB },
+  { nullptr,             "RazzleUndef",   1, RGB }
 };
 
 devInfo getDevice() {
@@ -65,95 +42,10 @@ devInfo getDevice() {
   return devices[i];  // the unmatched default is returned
 }
 
-const char* getHostname() {
-  return getDevice().hostname;
-}
-
 bool isRemote() {
   return getDevice().numLeds == 1;
 }
 
-int32_t httpGet(const char* url) {
-
-    HTTPClient http;
-
-    http.begin(url); //HTTP
-
-    int httpCode = http.GET();
-
-    // httpCode will be negative on error
-    if(httpCode > 0) {
-        // HTTP header has been send and Server response header has been handled
-
-        // file found at server
-        if(httpCode == HTTP_CODE_OK) {
-//            String payload = http.getString();
-//            Serial.println(payload);
-        }
-    }
-    Serial.printf("[HTTP] GET %s returned: %d , error: %s\n", url, httpCode, http.errorToString(httpCode).c_str());
-
-    http.end();
-
-    return httpCode;
-}
-
-void setupWifi() {
-  Serial.println("Begining setupWifi()");
-  Serial.printf("MAC address: %s\n", WiFi.macAddress().c_str());
-  Serial.printf("Hostname: %s\n", getHostname());
-  WiFi.mode(WIFI_STA);
-  WiFi.begin(ssid, password);
-  timeClient.begin();
-
-  // Port defaults to 8266
-  // ArduinoOTA.setPort(8266);
-
-  // Hostname defaults to esp8266-[ChipID]
-  // ArduinoOTA.setHostname("myesp8266");
-
-  // No authentication by default
-  // ArduinoOTA.setPassword((const char *)"123");
-
-  ArduinoOTA.setHostname(getHostname());
-
-  ArduinoOTA.onStart([]() {
-    Serial.println("Start");
-  });
-  ArduinoOTA.onEnd([]() {
-    Serial.println("\nEnd");
-  });
-  ArduinoOTA.onProgress([](unsigned int progress, unsigned int total) {
-    Serial.printf("Progress: %u%%\r", (progress / (total / 100)));
-  });
-  ArduinoOTA.onError([](ota_error_t error) {
-    Serial.printf("Error[%u]: ", error);
-    if (error == OTA_AUTH_ERROR) Serial.println("Auth Failed");
-    else if (error == OTA_BEGIN_ERROR) Serial.println("Begin Failed");
-    else if (error == OTA_CONNECT_ERROR) Serial.println("Connect Failed");
-    else if (error == OTA_RECEIVE_ERROR) Serial.println("Receive Failed");
-    else if (error == OTA_END_ERROR) Serial.println("End Failed");
-  });
-  ArduinoOTA.begin();
-}
-
-void loopWifi() {
-  bool wasUp = networkUp;
-  networkUp = WiFi.isConnected();
-
-  digitalWrite(BUILTIN_LED, !networkUp);  // false = LED on
-
-  if (wasUp != networkUp) {
-    WiFi.printDiag(Serial);
-    if (networkUp) {
-      Serial.println("Network up!");
-    } else {
-      Serial.println("Network Down!");
-    }
-  }
-
-  ArduinoOTA.handle();
-}
 
 void setup() {
   pinMode(BUILTIN_LED, OUTPUT);  // initialize onboard LED as output
@@ -167,17 +59,7 @@ void setup() {
   Serial.println("World!");
   setupLeds(getDevice().colorOrder, getDevice().numLeds);
 
-  setupWifi();
-
-//  if (getDevice().ambient_anode != getDevice().ambient_cathode) {
-//    ambient = new LEDAmbientSensor(getDevice().ambient_anode, getDevice().ambient_cathode);
-// }
-
-/*  pinMode(D5, OUTPUT);
-  pinMode(D0, OUTPUT);
-  digitalWrite(D5, true);
-  digitalWrite(D0, false);
-*/
+  setupWifi(getDevice().hostname, -7*60*60);
 }
 
 uint32_t lastDown = 0;
@@ -192,23 +74,10 @@ void loop()
 {
   static time_t lastTime = now();
   if (lastTime != now()) {
-    Serial.println(timeClient.getFormattedTime());
+    Serial.println(now());
     lastTime = now();
-
   }
 
-//  if (ambient) {
-//    static uint8_t lastAmbient = 0;
-//    uint8_t currAmbient = ambient->read();
-//    if (lastAmbient != currAmbient) {
-//      Serial.printf("Ambient: %d\n", currAmbient);
-//     lastAmbient = currAmbient;
-//    }
-//  } else {
-//    Serial.println("No ambient sensor");
-//  }
-
-  timeClient.update();
   button.poll();
   loopWifi();
 
